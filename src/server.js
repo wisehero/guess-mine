9466f023fe76754c754d528ca28a85d5255dc279
김지웅 <godjw96@gmail.com>
2022-03-12 17:27:22 +0900

    socketIO

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 import { join } from "path";
 import express from "express";
 import { Server, Socket } from "socket.io";
+import morgan from "morgan";
 
 const PORT = 4000;
 const app = express();
-
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(morgan("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res, next) => {
   return res.render("home");
@@ -17,4 +18,17 @@ const handleListening = () => {
 };
 
 const server = app.listen(PORT, handleListening);
+
 const io = new Server(server);
+
+io.on("connection", (socket) => {
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anon",
+    });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
+});
